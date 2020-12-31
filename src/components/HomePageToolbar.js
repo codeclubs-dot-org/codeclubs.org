f80799c80d5e14257259c73b282f2c09841a001a
Paul Perez <paul.perez@caringcent.com>
2020-12-31 13:37:33 -0700

    Adds new pages to repo

---
+++ src/components/HomePageToolbar.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import {
+  Box,
+  Button,
+  makeStyles
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  selectedButton: {
+    marginRight: theme.spacing(1),
+    textDecoration: 'underline',
+    "&:hover, &:focus": {
+      textDecoration: 'underline',
+    }
+  }
+}))
+
+const Toolbar = ({ className, ...rest }) => {
+  const classes = useStyles()
+  const path = window.location.pathname
+  return (
+    <div
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+      >
+        <RouterLink to="/mission">
+          <Button className={path === '/mission' && classes.selectedButton || classes.menuButton}>
+            Our Mission
+          </Button>
+        </RouterLink>
+        <RouterLink to="/contribute">
+          <Button className={path === '/contribute' && classes.selectedButton || classes.menuButton}>
+            Contribute
+          </Button>
+        </RouterLink>
+        <RouterLink to="/clubs">
+          <Button className={path === '/clubs' && classes.selectedButton || classes.menuButton}>
+            Clubs
+          </Button>
+        </RouterLink>
+        <RouterLink to="/register">
+          <Button
+            color="primary"
+            variant="contained"
+            classNAme={classes.menuButton}
+          >
+            Register
+          </Button>
+        </RouterLink>
+      </Box>
+    </div>
+  )
+}
+
+Toolbar.propTypes = {
+  className: PropTypes.string
+}
+
+export default Toolbar
